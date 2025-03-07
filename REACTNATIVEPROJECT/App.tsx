7769993f02400d67dca3ed70ae5f655050a5591d
0marKh1i1 <okkhalil23@cit.just.edu.jo>
2025-03-07 22:59:23 +0300

    added some libraries

--- REACTNATIVEPROJECT/App.tsx
+++ REACTNATIVEPROJECT/App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import {Task16 , Task17, Task18 , Task19, Task20, Task21, Task22, Task23, Task24, Task25, Task26, Task27, Task28, Task29, Task30, Task31, Task32} from './Tasks';
+import {Task16 , Task17, Task18 , Task19, Task20, Task21, Task22, Task23, Task24, Task25, Task26, Task27, Task28, Task29, Task30, Task31, Task32, Task33} from './Tasks';
 
 export default function App(): React.JSX.Element {
 
@@ -31,7 +31,8 @@ export default function App(): React.JSX.Element {
       {/* <Task29 /> */}
       {/* <Task30 /> */}
       {/* <Task31 /> */}
-      <Task32 />
+      {/* <Task32 /> */}
+      <Task33 />
     </View>
   );
 }
---
+++ REACTNATIVEPROJECT/react-native.config.js
@@ -0,0 +1,4 @@
+// filepath: c:\files\programming files\web\JoVision-react-native-tasks\REACTNATIVEPROJECT\react-native.config.js
+module.exports = {
+    assets: ['./node_modules/react-native-vector-icons/Fonts'],
+  };
